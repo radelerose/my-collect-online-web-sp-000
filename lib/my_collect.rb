64cc853d75130b21c6bd1890b88190f2683f914d
Radele Rose <radele.rose@gmail.com>
2021-03-03 02:34:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,9 @@
-array = ["Teddy", "Jim", "tony"]
-my_collect(array) do |name|
-  name.capitalizes
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
 end
